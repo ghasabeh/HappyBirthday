dc4933632dd74c35168b396b424c4453e2c4ddd6
Alireza Ghasabeie <alireza.ghasabeie@devolon.fi>
2024-02-16 13:42:41 +0330

    rename preview function

--- app/src/main/java/com/ghasabeie/happybirthday/MainActivity.kt
+++ app/src/main/java/com/ghasabeie/happybirthday/MainActivity.kt
@@ -39,7 +39,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true, showSystemUi = true, name= "Alireza 's preview")
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         Greeting("Android")
     }
