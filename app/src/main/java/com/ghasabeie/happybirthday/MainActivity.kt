73daf0ab08321fa0cfb8db0cfd56fe85f962bd8b
Alireza Ghasabeie <alireza.ghasabeie@devolon.fi>
2024-02-16 16:14:10 +0330

    change the image. use it as resource to background.

--- app/src/main/java/com/ghasabeie/happybirthday/MainActivity.kt
+++ app/src/main/java/com/ghasabeie/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ghasabeie.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,10 +58,15 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val backImage = painterResource(R.drawable.androidparty)
+    Image(painter = backImage, contentDescription = "$message $from")
+}
 @Preview(showBackground = true, showSystemUi = true, name = "Alireza 's preview")
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Alireza!", from = "From Android")
+        GreetingImage(message = "Happy Birthday Alireza!", from = "From Android")
     }
 }
